9ff7bbdfbe8a139d7c0e9a217bc92a886eee1899
tinabayi <bayizerechristine4@gmail.com>
2019-05-05 07:04:44 +0200

    i make change

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,31 @@
 $(document).ready(function() {
     $("h2").click(function() {
-        $(".h2").toggle();
+        $("h2").toggle();
         $(".quiz").toggle();
+    });
+    $("h2").click(function() {
+        $("h2").toggle();
+        $(".search").toggle();
+
+    });
+    $("h2").click(function() {
+        $("h2").toggle();
+        $(".quote").toggle();
+
+    });
+    $("h2").click(function() {
+        $("h2").toggle();
+        $(".gallery").toggle();
+
+    });
+    $("h2").click(function() {
+        $("h2").toggle();
+        $(".news").toggle();
+
+    });
+    $("h2").click(function() {
+        $("h2").toggle();
+        $(".blog").toggle();
+
     });
   });
